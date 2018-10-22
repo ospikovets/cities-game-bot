db11dfb5effd19af33232c039f35556987b924c7
Oleksandr PIkovets <opikovets@spscommerce.com>
2018-10-22 22:28:30 +0300

    Fix linter and add typing

--- cities_game/game_engine.py
+++ cities_game/game_engine.py
@@ -1,17 +1,22 @@
+"""Game engine"""
+
 from collections import deque
 import signal
-import typing
+from typing import (
+    List,
+    Set,
+)
 
 
 class CitiesGameEngine:
     """Class to describe game engine
 
     This class encapsulates all game logic
-    
+
     Args:
         players (list): List of players, who plays the game.
         cities (set): Cities awailable for the game.
-        thinking_time (:obj:`int`, optional): Time in secconds 
+        thinking_time (:obj:`int`, optional): Time in secconds
             to think about the next word. Defaults to 60.
     Attributes:
         players (list): List of players, who plays the game.
@@ -19,7 +24,7 @@ class CitiesGameEngine:
         thinking_time (int): Time in secconds to think about the next word.
     """
 
-    def __init__(self, players, cities, thinking_time=60):
+    def __init__(self, players: List, cities: Set, thinking_time: int = 60):
         self.players = players
         self.cities = cities
         self.thinking_time = thinking_time
@@ -27,12 +32,12 @@ class CitiesGameEngine:
         self._is_playing = False
         self._used_cities = set()
         self._lost_players = []
-    
+
     def start(self):
         """Start new game
-        
+
         Returns:
-            
+            list: Of the players, who didn't loose the game.
         """
 
         self._is_playing = True
@@ -50,14 +55,14 @@ class CitiesGameEngine:
                 self._used_cities.add(word)
                 letter = word[-1]
                 players_in_game.append(curr_player)
-        
+
         return self.finish()
 
     def finish(self):
         """Finish the game and name the winner(s)
-        
+
         Returns:
-            set: of players, who didn't loose
+            list: Of the players, who didn't loose the game.
         """
 
         self._is_playing = False
@@ -68,21 +73,21 @@ class CitiesGameEngine:
 
         if self._is_playing:
             self.finish()
-        
+
         self._used_cities = set()
         self._lost_players = []
         self.start()
 
-    def is_valid(self, word, letter):
+    def is_valid(self, word: str, letter: str) -> bool:
         """Validate the word
-        
+
         Check if the word starts from the correct letter
         and is a valid city from the list,
         and if it wasn't already mentioned by other players.
 
         Args:
-            word (str): The word to validate
-            letter (str): The expected letter to start the word from
+            word (str): The word to validate.
+            letter (str): The expected letter to start the word from.
 
         Returns:
             bool: True if valid, False otherwise.
@@ -94,11 +99,11 @@ class CitiesGameEngine:
 
         return starts_correctly and is_valid_city and not mentioned
 
-    def get_next_word(self, player, letter):
+    def get_next_word(self, player, letter: str) -> str:
         """Get the word from the player
-        
+
         Ask the player for the next word,
-        while there is no valid word given 
+        while there is no valid word given
         or timeout is reached.
 
         Returns:
@@ -107,12 +112,12 @@ class CitiesGameEngine:
             TimeoutError: If the word wasn't gotten during the `thinkg_time`
         """
 
-        signal.signal(signal.SIGALRM, raise_timeout)
+        signal.signal(signal.SIGALRM, lambda *args: raise_(TimeoutError))
         signal.alarm(self.thinking_time)
 
         word = player.get_input(f'Your turn and the letter is "{letter}"')
         while not self.is_valid(word, letter):
-            word = player.get_input('This word is not valid! ' 
+            word = player.get_input('This word is not valid! '
                                     'Please give another one!')
 
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
@@ -120,5 +125,9 @@ class CitiesGameEngine:
         return word
 
 
-def raise_timeout(signum, frame):
-    raise TimeoutError
+def raise_(ex):
+    """Raise an Exception
+
+    Wrapper to raise an exception in lambda function
+    """
+    raise ex
